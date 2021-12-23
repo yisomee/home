6dcd09ce044773222797c6196a5814f3d487e088
sys <ysshin@yellotail.com>
2021-12-23 13:41:27 +0900

    index

---
+++ src/test/java/com/home/yellotail/YellotailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.yellotail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellotailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
