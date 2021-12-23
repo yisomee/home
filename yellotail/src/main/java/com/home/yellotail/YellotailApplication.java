25dbe9ca9c33b7971ad6cf64a4a2bc30c76486fd
yisomee <77676596+yisomee@users.noreply.github.com>
2021-12-23 13:43:29 +0900

    index

---
+++ yellotail/src/main/java/com/home/yellotail/YellotailApplication.java
@@ -0,0 +1,13 @@
+package com.home.yellotail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellotailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellotailApplication.class, args);
+	}
+
+}
